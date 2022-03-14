0dfca88e4fbf5135137e745c644a4a7c21684859
TalashV <talashbig_34@mail.ru>
2022-03-14 13:46:08 +0300

    from work

---
+++ loops and iteration.py
@@ -0,0 +1,55 @@
+n = 5
+while n > 0 :
+  print (n)
+  n = n - 1
+print ('Blastoff!')
+print (n)
+
+
+# break and continue
+
+while True:
+  line = input("> ")
+  if line [0] == "#" :
+    continue
+  if line == 'done' :
+    break
+  print (line)
+print('Done!')
+
+
+#definite loops (for)
+
+for i in [5,4,3,2,1] :
+    print (i)
+print ('something')
+
+#finding larges
+t value
+
+largest_so_far = -1
+print ('Before', largest_so_far)
+for thing in [9,41,56,231,512,3,21,0]
+    if thing > largest_so_far :
+        largest_so_far = thing
+    print(largest_so_far, thing)
+
+print("After", largest_so_far)
+
+
+#look idioms
+
+count = 0
+print ("Before", count)
+for thing in [9,3,3,5,7,11] :
+    count = count + 1
+    print (count, thing)
+print("After", count)
+
+#total summing
+
+... for thing in ...
+    count = count + thing
+    print (count, thing)
+
+#boolean variable true or false
